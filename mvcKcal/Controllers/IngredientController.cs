20afcfe2af1db2d7e45271927bafd06d2adbeb17
Dom Hall <domjshall@gmail.com>
2024-08-12 11:38:53 +0100

    ingredient index

---
+++ mvcKcal/Controllers/IngredientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvcKcal.Controllers
+{
+    public class IngredientController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Ingredient> ingredients = new List<Ingredient>();
+            ingredients.Add(new Ingredient()
+            {
+                Id = 1,
+                Name = "ingredient 1",
+                Protein = 1,
+                Carbs = 1,
+                Fats = 1,
+            });
+            return View(ingredients);
+        }
+    }
+}
---
+++ mvcKcal/Models/IngredientListItem.cs
@@ -0,0 +1,5 @@
+public class IngredientListItem()
+{
+    public int Quantity { get; set; }
+    public Ingredient Ingredient { get; set; }
+}
