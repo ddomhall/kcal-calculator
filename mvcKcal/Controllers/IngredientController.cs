2840426a30ab416b1d87345257b2ca2401b3ff7d
ddomhall <domjshall@gmail.com>
2024-08-13 17:45:29 +0100

    ingredientservice

--- mvcKcal/Controllers/IngredientController.cs
+++ mvcKcal/Controllers/IngredientController.cs
@@ -6,15 +6,7 @@ namespace mvcKcal.Controllers
     {
         public IActionResult Index()
         {
-            List<Ingredient> ingredients = new List<Ingredient>();
-            ingredients.Add(new Ingredient()
-            {
-                Id = 1,
-                Name = "ingredient 1",
-                Protein = 1,
-                Carbs = 1,
-                Fats = 1,
-            });
+            List<Ingredient> ingredients = IngredientService.GetAll();
             return View(ingredients);
         }
     }
---
+++ mvcKcal/Services/IngredientService.cs
@@ -0,0 +1,17 @@
+﻿namespace mvcKcal;
+
+public class IngredientService
+{
+    static List<Ingredient> ingredients { get; }
+
+    static IngredientService()
+    {
+        ingredients = new List<Ingredient>
+        {
+            new Ingredient {Id = 0, Name = "ingredient0", Protein = 0, Carbs = 0, Fats = 0},
+            new Ingredient {Id = 1, Name = "ingredient1", Protein = 1, Carbs = 1, Fats = 1},
+        };
+    }
+
+    public static List<Ingredient> GetAll() => ingredients;
+}
